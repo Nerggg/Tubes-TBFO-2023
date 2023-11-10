193f3fdbd75c4e26e119bddaebfc13a0602e025a
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-11-10 10:58:41 +0700

    feat: wip pda and fix: pda algorithm reject on last html tag

--- src/main.py
+++ src/main.py
@@ -20,17 +20,29 @@ passed = False
 alpha = False
 temp = str()
 
+# skip newline yang ada
+while (html[i] == '\n'):
+    i += 1
+
 while (i < len(html) - 1 and reject == False):
     print(f"html nya pas i {i} adalah {html[i]}")
     print(f"dan current statenya adalah {state}")
     print(f"dan stacknya adalah {stack}")
     print()
-    if (html[i] != '<' and stack[len(stack)-1] == 'G'):
-        if (html[i] == '\n'):
-            print(f"pas newline dia masuk sini")
+    if (len(html) - i > 8 and (html[i] == '\n' or html[i] == '\t' or html[i] == ' ') and (stack[len(stack)-1] == 'H' or stack[len(stack)-1] == 'T' or stack[len(stack)-1] == 'B' or stack[len(stack)-1] == 'S')):
+#        print("masuk atas")
         i += 1
         continue
-    elif (html[i] in alphabet): # todo: yang masih rusak itu dibagian akusisi kata2 alphabetnya
+    elif (len(html) - i > 8 and stack[len(stack)-1] == '<' and state == 'W' and (html[i] == '\n' or html[i] == '\t' or html[i] == ' ')):
+#        print("masuk bawah")
+        i += 1
+        continue
+    elif (html[i] != '<' and stack[len(stack)-1] == 'G'):
+#        if (html[i] == '\n'):
+#            print(f"pas newline dia masuk sini")
+        i += 1
+        continue
+    elif (html[i] in alphabet):
         while (html[i] in alphabet):
             temp = temp + html[i]
             alpha = True
@@ -50,6 +62,7 @@ while (i < len(html) - 1 and reject == False):
         reject = True
     passed = False
     j = 0
+    print(f"temp nya {temp}")
     temp = str()
     if (not(alpha)):
         i += 1
@@ -59,4 +72,8 @@ while (i < len(html) - 1 and reject == False):
 if (reject == False and len(stack) == 1 and stack[0] == 'Z'):
     print("accept")
 else:
+    print(html[i:])
+    print(f"html nya pas {i} adalah {html[i]}")
+    print(f"stack nya {stack}")
+    print(f"state nya {state}")
     print(f"reject di i {i}")
